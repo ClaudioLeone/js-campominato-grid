0d9bd360e4e489a47ffa86a1725fa73bd05d7da4
ClaudioLeone <claleone92@gmail.com>
2023-03-17 16:00:19 +0100

    Created grid [non-dynamic]

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+/* 
+   -    L'utente clicca su un bottone per generare una griglia quadrata
+   -    Ogni cella ha un numero progressivo da 1 a 100 (inclusi)
+   -    La griglia deve essere 10 x 10
+   -    Al click su ogni casella:
+        -   La casella diventa azzura
+        -   In console deve essere riportato il nr appartenente alla casella cliccata
+*/
+
